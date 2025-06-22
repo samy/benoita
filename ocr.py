a375ffc517ad89f01d6fb4f00e7fff34e9fd63db
Samy Rabih <contact@lahaut.info>
2025-06-22 20:30:16 +0200

    Export to txt file

--- ocr.py
+++ ocr.py
@@ -1,15 +1,21 @@
 import pytesseract
 from pypdf import PdfWriter
+from pytesseract import image_to_string
 from tqdm import tqdm
 import os
+from time import sleep
 
 imagesFolder = 'images/pages'
 open('out.pdf', 'a').close()
+open('out.txt', 'a').close()
 
 fileCounter = 0
 for entry in tqdm(os.scandir(imagesFolder), desc="Progression", total=len(os.listdir(imagesFolder))):
+    sleep(0.05)
     filename = entry.path
-    pdf = pytesseract.image_to_pdf_or_hocr(filename, extension='pdf', lang='fra')
+    pdf = pytesseract.image_to_pdf_or_hocr(filename, extension='pdf', lang='fra', config='-c textonlypdf=0')
+    with open('out.txt', 'a') as f:
+        f.write(image_to_string(filename))  # pdf type is bytes by default
     with open('output/test' + str(fileCounter) + '.pdf', 'w+b') as f:
         f.write(pdf)  # pdf type is bytes by default
     fileCounter = fileCounter + 1
