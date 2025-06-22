21aba8299519f2e05197cb42d47459b152010ff8
Samy Rabih <contact@lahaut.info>
2025-06-22 17:19:17 +0200

    Script upgraded to handle folder scan

--- ocr.py
+++ ocr.py
@@ -1,20 +1,24 @@
 import pytesseract
 from pypdf import PdfWriter
+from tqdm import tqdm
 import os
 
-filename = 'image_01.jpg'
-pdf = pytesseract.image_to_pdf_or_hocr(filename, extension='pdf', lang='fra')
-open('test.pdf', 'a').close()
-with open('test.pdf', 'w+b') as f:
-    f.write(pdf) # pdf type is bytes by default
-with open('test2.pdf', 'a+b') as f:
-    f.write(pdf) # pdf type is bytes by default
+imagesFolder = 'images/pages'
+open('out.pdf', 'a').close()
+
+fileCounter = 0
+for entry in tqdm(os.scandir(imagesFolder), desc="Progression", total=len(os.listdir(imagesFolder))):
+    filename = entry.path
+    pdf = pytesseract.image_to_pdf_or_hocr(filename, extension='pdf', lang='fra')
+    with open('output/test' + str(fileCounter) + '.pdf', 'w+b') as f:
+        f.write(pdf)  # pdf type is bytes by default
+    fileCounter = fileCounter + 1
 
 merger = PdfWriter()
 
-for pdf in ["test.pdf", "test2.pdf"]:
-    merger.append(pdf)
-    os.remove(pdf)
+for pdf in os.scandir('output'):
+     merger.append(str(pdf.path))
+     os.remove(pdf.path)
 
 merger.write("out.pdf")
 merger.close()
