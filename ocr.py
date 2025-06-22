01e0692e32acc2f5c51e6a0d13c19d6367f4cc83
Samy Rabih <contact@lahaut.info>
2025-06-22 10:27:39 +0200

    Improvement of PDF generation

--- ocr.py
+++ ocr.py
@@ -1,10 +1,20 @@
-from PIL import Image
-
 import pytesseract
-from pytesseract import Output
+from pypdf import PdfWriter
+import os
 
 filename = 'image_01.jpg'
-img1 = np.array(Image.open(filename))
 pdf = pytesseract.image_to_pdf_or_hocr(filename, extension='pdf', lang='fra')
+open('test.pdf', 'a').close()
 with open('test.pdf', 'w+b') as f:
     f.write(pdf) # pdf type is bytes by default
+with open('test2.pdf', 'a+b') as f:
+    f.write(pdf) # pdf type is bytes by default
+
+merger = PdfWriter()
+
+for pdf in ["test.pdf", "test2.pdf"]:
+    merger.append(pdf)
+    os.remove(pdf)
+
+merger.write("out.pdf")
+merger.close()
